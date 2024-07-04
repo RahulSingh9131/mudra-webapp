6ec950d6d7e39277af0859454067a0e514dc16f1
K Srinivas Rao <ksrinivasrao531@gmail.com>
2024-07-04 08:24:39 +0530

    testing prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  trailingComma: "es5",
+  plugins: ['prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
   bracketSpacing: true,
   printWidth: 80,
   tabWidth: 2,
   singleQuote: true,
-  arrowParens: "always",
+  arrowParens: 'always',
   semi: true,
 };
 
