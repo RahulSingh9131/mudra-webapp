869477e4f78f528c81116ca5126a69f53f79fcd6
RahulSingh9131 <singh.rahul3111998vipin@gmail.com>
2024-07-21 21:35:53 +0530

    feat: add layout wrapper

---
+++ src/app/(dashboard)/income/page.tsx
@@ -0,0 +1,16 @@
+import LayoutWrapper from '@/app/_layouts/layout-wrapper';
+import React from 'react';
+
+const Income = () => {
+  return (
+    <LayoutWrapper>
+      <main className="h-full w-full rounded-3xl">
+        <div className="p-5">
+          <h1 className="text-2xl font-bold text-foreground">Income</h1>
+        </div>
+      </main>
+    </LayoutWrapper>
+  );
+};
+
+export default Income;
